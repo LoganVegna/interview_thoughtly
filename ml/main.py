16de461d03c7c271e9942acab336c3fea077d303
Logan Vegna <lvegna12@gmail.com>
2024-03-29 17:08:14 -0400

    Remove Access Key

--- ml/main.py
+++ ml/main.py
@@ -29,7 +29,7 @@ import requests
 
 # This will use the huggingface API to query Mixtral-8x7 instead which works alot better. You will need your own access token
 API_URL = "https://api-inference.huggingface.co/models/mistralai/Mixtral-8x7B-Instruct-v0.1"
-headers = {"Authorization": "Bearer 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"}
+headers = {"Authorization": "Bearer "}
 
 def query(payload):
     response = requests.post(API_URL, headers=headers, json=payload)
